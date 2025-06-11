137d21f132a76a6e240a3a35cf24bfc70026b3b7
digiot <digiot.abc@gmail.com>
2025-06-11 23:45:58 +0900

    Initial Commit

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,15 @@
 <script>
 	import { t } from '$lib/i18n';
 	import { page } from '$app/stores';
+	import { get } from 'svelte/store';
 	import { agencyCode } from '$lib/agency/agencyCode';
 
 	$: path = $page.url.pathname;
 
 	// ✅ 非表示にしたい条件：/cart や /products/（ただし /products 単体はOK）
 	$: shouldHideFooter = path === '/cart' || path.startsWith('/products/') ;
+
+	$: shopCode = get(agencyCode);
 </script>
 
 <footer class={`text-sm text-center py-8 sm:py-12 ${path === '/cart' ? 'pb-24' : 'pb-12'} mt-12`}>
@@ -17,8 +20,8 @@
 <!--				TODO-->
 <!--				<a href="/privacy" class="hover:underline">{t('footer.privacy')}</a>-->
 <!--				<a href="/terms" class="hover:underline">{t('footer.terms')}</a>-->
-				{#if agencyCode}
-					<a href={`https://liff.line.me/2007233135-vxLAjokB?shopCode=${agencyCode}`}>お問い合わせ</a>
+				{#if shopCode}
+					<a href={`https://liff.line.me/2007233135-vxLAjokB?shopCode=${shopCode}`}>お問い合わせ</a>
 				{/if}
 			</nav>
 		</div>
